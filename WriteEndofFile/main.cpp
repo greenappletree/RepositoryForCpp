8361bdb7b783513ae0d17c043ce209aff4a35364
wenshanwang <wenshanfly@126.com>
2013-04-22 23:02:14 +0800

    Demo for Write the end of file
    Demo for Write the end of file

---
+++ WriteEndofFile/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main()
+{
+    //cout << "Hello world!" << endl;
+    FILE *fp;
+    fp=fopen("file.txt","a+");
+    //fseek(fp);
+    //cout<<"4 5 6"<<endl;
+    fprintf(fp,"4 5 6");
+    fclose(fp);
+    return 0;
+}
